4244fa5f6fef19dc09a0f576a4ac5f22b37d29d4
Alexandru Branza <branza.alex@gmail.com>
2024-05-29 19:45:37 +0300

    Wrap in Promise

--- index.js
+++ index.js
@@ -9,102 +9,107 @@ const defaults = {
   bytesLimit: 20 * 1024 * 1024,
 }
 
-async function init(opts = {}) {
-  const url = opts.url || defaults.url
-  const chunkSize = opts.chunkSize || defaults.chunkSize
-  let bytesLimit = opts.bytesLimit || defaults.bytesLimit
+function mp4tracks(opts = {}) {
+  return new Promise(async function(resolve, reject) {
+    const url = opts.url || defaults.url
+    const chunkSize = opts.chunkSize || defaults.chunkSize
+    let bytesLimit = opts.bytesLimit || defaults.bytesLimit
 
-  const mp4boxFile = MP4Box.createFile()
+    const mp4boxFile = MP4Box.createFile()
 
-  let stopped = false
+    let stopped = false
 
-  mp4boxFile.onError = function(err) {
-    stopped = true
-    console.error(err)
-  }
+    mp4boxFile.onError = function(err) {
+      stopped = true
+      console.error(err)
+      reject(err)
+    }
 
-  mp4boxFile.onReady = function(videoData) {
-    stopped = true
-    console.log('onready', videoData)
-  }
+    mp4boxFile.onReady = function(videoData) {
+      stopped = true
+      console.log('onready', videoData)
+      resolve(videoData)
+    }
 
-  let offset = 0
-  let start = 0
-  let end = chunkSize
+    let offset = 0
+    let start = 0
+    let end = chunkSize
 
-  const fileMedia = await urlToFileMedia(url)
+    const fileMedia = await urlToFileMedia(url)
 
-  async function findMoov(startFrom) {
-    let hOffset = startFrom || 0
-    for (let k = 0; k < 5; k++) {
-      if (stopped)
-        return { error: true }
+    async function findMoov(startFrom) {
+      let hOffset = startFrom || 0
+      for (let k = 0; k < 5; k++) {
+        if (stopped)
+          return { error: true }
 
-      let hEnd = Math.min(hOffset + chunkSize, fileMedia.length)
+        let hEnd = Math.min(hOffset + chunkSize, fileMedia.length)
 
-      if (hEnd > bytesLimit)
-        return { error: true }
+        if (hEnd > bytesLimit)
+          return { error: true }
 
-      const fileStream = await fileMedia.createReadStream({ start: hOffset, end: hEnd })
-      const buffer = await streamToBuffer(fileStream)
+        const fileStream = await fileMedia.createReadStream({ start: hOffset, end: hEnd })
+        const buffer = await streamToBuffer(fileStream)
+        const arrayBuffer = toArrayBuffer(buffer, offset)
+
+        start = offset = mp4boxFile.appendBuffer(arrayBuffer)
+
+        if (hEnd === fileMedia.length)
+          return { error: true }
+
+        let result =  buffer.indexOf('moov')
+        if (result > 0) {
+          return {
+            offset: hOffset + result - 4,
+            size: buffer.readUInt32BE(result - 4),
+          }
+        } else if (!startFrom && start > 5 * chunkSize) {
+          // moov is at end of file
+          bytesLimit = start + bytesLimit - hEnd
+          return findMoov(start)
+        }
+
+        hOffset += chunkSize
+      }
+      return { error: true }
+    }
+
+    const moov = await findMoov()
+
+    end = !moov.error ? moov.offset + moov.size : start + chunkSize
+
+    async function loopStream() {
+      if (stopped) return
+
+      if (end > bytesLimit) {
+        reject(Error('Reached bytes limit'))
+        console.log('Reached bytes limit')
+        return
+      }
+
+      if (start >= fileMedia.length) {
+        reject(Error('Passed end of file'))
+        console.log('Passed end of file')
+        return
+      }
+
+      const stream = await fileMedia.createReadStream({ start, end })
+      const buffer = await streamToBuffer(stream)
       const arrayBuffer = toArrayBuffer(buffer, offset)
 
       start = offset = mp4boxFile.appendBuffer(arrayBuffer)
-
-      if (hEnd === fileMedia.length)
-        return { error: true }
-
-      let result =  buffer.indexOf('moov')
-      if (result > 0) {
-        return {
-          offset: hOffset + result - 4,
-          size: buffer.readUInt32BE(result - 4),
-        }
-      } else if (!startFrom && start > 5 * chunkSize) {
-        // moov is at end of file
-        bytesLimit = start + bytesLimit - hEnd
-        return findMoov(start)
-      }
-
-      hOffset += chunkSize
-    }
-    return { error: true }
-  }
-
-  const moov = await findMoov()
-
-  end = !moov.error ? moov.offset + moov.size : start + chunkSize
-
-  async function loopStream() {
-    if (stopped) return
-
-    if (end > bytesLimit) {
-      console.log('Reached bytes limit')
-      return
-    }
-
-    const stream = await fileMedia.createReadStream({ start, end })
-    const buffer = await streamToBuffer(stream)
-    const arrayBuffer = toArrayBuffer(buffer, offset)
-
-    start = offset = mp4boxFile.appendBuffer(arrayBuffer)
-
-    if (end !== fileMedia.length) {
       end = Math.min(offset + chunkSize, fileMedia.length)
-    } else {
-      console.log('Reached end of file')
-      return
+
+      loopStream()
     }
 
-    loopStream()
-  }
-
-  if (start < fileMedia.length)
-    loopStream()
+    if (start < fileMedia.length)
+      loopStream()
+  })
 }
 
 if (require.main === module) {
-  init()
+  mp4tracks()
 } else {
-  module.exports = init
+  module.exports = mp4tracks
 }
